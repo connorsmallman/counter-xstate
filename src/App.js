106aa28258e42e6891dc061a936947ae2da7a912
Connor Smallman <connor.smallman@bedegaming.com>
2018-04-16 13:55:20 +0100

    Added new double state

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { withStatechart } from 'react-automata';
 
 const isNotZero = count => count > 0;
 
-const DEFAULT_STATE = { count: 0 };
+const DEFAULT_STATE = { count: 0, isDoubleRate: false };
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
+const RESET = 'RESET';
+const TOGGLE_DOUBLE = 'TOGGLE_DOUBLE';
 
 const statechart = {
   initial: 'start',
@@ -22,7 +24,29 @@ const statechart = {
             actions: ['decrement'],
             cond: isNotZero
           }
-        }
+        },
+        RESET: {
+          start: {
+            actions: ['reset']
+          }
+        },
+        TOGGLE_DOUBLE: 'double'
+      }
+    },
+    double: {
+      on: {
+        INCREMENT: {
+          double: {
+            actions: ['incrementDouble']
+          }
+        },
+        DECREMENT: {
+          double: {
+            actions: ['decrementDouble'],
+            cond: isNotZero
+          }
+        },
+        TOGGLE_DOUBLE: 'start'
       }
     }
   }
@@ -33,6 +57,8 @@ class App extends Component {
 
   handleOnIncrement = this.handleOnIncrement.bind(this);
   handleOnDecrement = this.handleOnDecrement.bind(this);
+  handleOnReset = this.handleOnReset.bind(this);
+  handleToggleDouble = this.handleToggleDouble.bind(this);
 
   increment() {
     this.setState({ count: this.state.count + 1 });
@@ -42,12 +68,33 @@ class App extends Component {
     this.setState({ count: this.state.count - 1 });
   }
 
+  incrementDouble() {
+    this.setState({ count: this.state.count + 2 });
+  }
+
+  decrementDouble() {
+    this.setState({ count: this.state.count - 2 });
+  }
+
+  reset() {
+    this.setState(DEFAULT_STATE);
+  }
+
   handleOnIncrement() {
     this.props.transition(INCREMENT);
   }
 
   handleOnDecrement() {
-    this.props.transition(DECREMENT, this.state.count);
+    this.props.transition(DECREMENT);
+  }
+
+  handleOnReset() {
+    this.props.transition(RESET);
+  }
+
+  handleToggleDouble() {
+    this.setState({ isDoubleRate: !this.state.isDoubleRate }, () => 
+      this.props.transition(TOGGLE_DOUBLE));
   }
 
   render() {
@@ -56,6 +103,8 @@ class App extends Component {
         <button onClick={this.handleOnIncrement}>+</button>
         <span>{this.state.count}</span>
         <button onClick={this.handleOnDecrement}>-</button>
+        <button onClick={this.handleOnReset}>Reset</button>
+        <button onClick={this.handleToggleDouble}>Double: {this.state.isDoubleRate ? 'On' : 'Off'}</button>
       </div>
     );
   }
